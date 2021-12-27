3f1d58a873dbb12911ed364f9f3112efe834d45f
Adam Bryt <adam.bryt@gmx.com>
2021-12-27 22:48:09 +0100

    day12: part 1: refactoring

--- day12/day12.adb
+++ day12/day12.adb
@@ -4,6 +4,7 @@ with Ada.Text_IO;              use Ada.Text_IO;
 with Ada.Text_IO.Unbounded_IO; use Ada.Text_IO.Unbounded_IO;
 with Ada.Command_Line;         use Ada.Command_Line;
 with Ada.Strings.Unbounded;    use Ada.Strings.Unbounded;
+with Ada.Strings.Fixed;        use Ada.Strings.Fixed;
 with Ada.Containers.Vectors;   use Ada.Containers;
 with Ada.Characters.Handling;  use Ada.Characters.Handling;
 
@@ -11,185 +12,177 @@ procedure Day12 is
    
    Value_Error : exception;
    
-   subtype Node_Index is Positive;
+   type Node_Type;
+   type Node_Access is access Node_Type;
    
-   package Neighbor_Vector is new Ada.Containers.Vectors
-     (Index_Type => Positive,
-      Element_Type => Node_Index);
+   package Node_Vector is new Ada.Containers.Vectors
+     (Index_Type   => Positive,
+      Element_Type => Node_Access);
    
    type Node_Type is
       record
-         Name : Unbounded_String;
-         Visited : Boolean := False;
-         Neighbors : Neighbor_Vector.Vector;
+         Name      : Unbounded_String;
+         Small     : Boolean;
+         Visited   : Natural := 0;
+         Neighbors : Node_Vector.Vector;
       end record;
-   
-   package Node_Vector is new Ada.Containers.Vectors
-     (Index_Type => Node_Index,
-      Element_Type => Node_Type);
-   
-   procedure Print_Node (Node : Node_Type) is
-   begin
-      Put_Line ("Name: " & To_String(Node.Name));
-      Put_Line ("Num of neighbors:" & Node.Neighbors.Length'Img);
-   end Print_Node;
    
    procedure Print_Nodes (Nodes : Node_Vector.Vector) is
    begin
-      for I in Nodes.First_Index .. Nodes.Last_Index loop
-         Put_Line ("Node index:" & I'Img);
-         Put_Line ("Node name: " & To_String (Nodes.Element(I).Name));
-         Put_Line ("Node neighbors:");
-         for N of Nodes.Element(I).Neighbors loop
-            Put (N'Img);
+      for N of Nodes loop
+         Put_Line (N.Name);
+         Put ("  ");
+         for E of N.Neighbors loop
+            Put (E.Name);
+            Put (", ");
          end loop;
          New_Line;
       end loop;
    end Print_Nodes;
-   
-   function Is_Small (Node : Node_Type) return Boolean is
+         
+   function Contains_Node (Nodes : Node_Vector.Vector;
+                           Name : Unbounded_String) return Boolean is
    begin
-      for I in 1 .. Length (Node.Name) loop
-         if Is_Upper (Element (Node.Name, I)) then
+      for N of Nodes loop
+         if N.Name = Name then
+            return True;
+         end if;
+      end loop;
+      return False;
+   end Contains_Node;
+   
+   function Find_Node (Nodes : Node_Vector.Vector;
+                       Name : Unbounded_String) return Node_Access is
+   begin
+      for N of Nodes loop
+         if N.Name = Name then
+            return N;
+         end if;
+      end loop;
+      raise Value_Error with "Missing node: " & To_String (Name);
+   end Find_Node;
+         
+   function Is_Small_Case (Name : Unbounded_String) return Boolean is
+   begin
+      for I in 1 .. Length (Name) loop
+         if Is_Upper (Element (Name, I)) then
             return False;
          end if;
       end loop;
       return True;
-   end Is_Small;
+   end Is_Small_Case;
 
    procedure Read_Data (File_Name : String; Nodes : out Node_Vector.Vector) is
-      File : File_Type;
       
-      procedure Parse_Edge (Line : String; N1, N2 : out Node_Type) is
-         Sep : constant Character := '-';
-         Idx : Positive := Line'First;
+      procedure Node_Names (Line : String;
+                            Name1, Name2 : out Unbounded_String)
+      is
+         Separator : constant String := "-";
+         I : Natural := Index (Line, Separator);
       begin
-         while Line(Idx) /= Sep loop
-            N1.Name := N1.Name & Line (Idx);
-            Idx := Idx + 1;
-         end loop;
-         
-         Idx := Idx + 1; -- skip separator
-         
-         while Idx <= Line'Last loop
-            N2.Name := N2.Name & Line (Idx);
-            Idx := Idx + 1;
-         end loop;
-      end Parse_Edge;
-      
-      function Contains (Nodes : Node_Vector.Vector;
-                         Node : Node_Type) return Boolean is
-      begin
-         for N of Nodes loop
-            if N.Name = Node.Name then
-               return True;
-            end if;
-         end loop;
-         return False;
-      end Contains;
-      
-      function Index (Nodes : Node_Vector.Vector;
-                      Node : Node_Type) return Natural is
-      begin
-         for I in Nodes.First_Index .. Nodes.Last_Index loop
-            if Nodes.Element(I).Name = Node.Name then
-               return I;
-            end if;
-         end loop;
-         return 0;
-      end Index;
+         if I = 0 then
+            raise Value_Error with "Missing node separator: '" & Line & "'";
+         end if;
+         Name1 := To_Unbounded_String
+           (Line (Line'First .. I - 1));
+         Name2 := To_Unbounded_String
+           (Line (I + Separator'Length .. Line'Last));
+      end Node_Names;
       
       procedure Add_Edge (Nodes : in out Node_Vector.Vector;
-                          N1, N2 : Node_Type)
-      is
-         N1_Idx, N2_Idx : Node_Index;
+                          Name1, Name2 : Unbounded_String) is
+
+         Node1, Node2 : Node_Access;
       begin
-         if Contains (Nodes, N1) then
-            N1_Idx := Index (Nodes, N1);
+         if Contains_Node (Nodes, Name1) then
+            Node1 := Find_Node (Nodes, Name1);
          else
-            Nodes.Append (N1);
-            N1_Idx := Nodes.Last_Index;
+            Node1 := new Node_Type;
+            Node1.Name := Name1;
+            Node1.Small := Is_Small_Case (Name1);
+            Nodes.Append (Node1);
          end if;
          
-         if Contains (Nodes, N2) then
-            N2_Idx := Index (Nodes, N2);
+         if Contains_Node (Nodes, Name2) then
+            Node2 := Find_Node (Nodes, Name2);
          else
-            Nodes.Append (N2);
-            N2_Idx := Nodes.Last_Index;
+            Node2 := new Node_Type;
+            Node2.Name := Name2;
+            Node2.Small := Is_Small_Case (Name2);
+            Nodes.Append (Node2);
          end if;
          
-         Nodes.Reference(N1_Idx).Neighbors.Append (N2_Idx);
-         Nodes.Reference(N2_Idx).Neighbors.Append (N1_Idx);
+         Node1.Neighbors.Append (Node2);
+         Node2.Neighbors.Append (Node1);
       end Add_Edge;
       
-   -- Start of processing for Read_Data
+   begin
+      declare
+         File : File_Type;
+      begin
+         Open (File, In_File, File_Name);
+         while not End_Of_File (File) loop
+            declare
+               Line   : String := Get_Line (File);
+               Name1, Name2 : Unbounded_String;
+            begin
+               Node_Names (Line, Name1, Name2);
+               Add_Edge (Nodes, Name1, Name2);
+            end;
+         end loop;
+         Close (File);
+      end;
+   end Read_Data;
+   
+   procedure Search_Paths (Nodes : Node_Vector.Vector;
+                           Node : Node_Access;
+                           Paths : in out Natural) is
+      
+      function Is_Start (Node : Node_Access) return Boolean is
+      begin
+         return To_String (Node.Name) = "start";
+      end Is_Start;
+      
+      function Is_End (Node : Node_Access) return Boolean is
+      begin
+         return To_String (Node.Name) = "end";
+      end Is_End;
 
    begin
-      Open (File, In_File, File_Name);
-      while not End_Of_File (File) loop
-         declare
-            Line : String := Get_Line (File);
-            N1, N2 : Node_Type;
-         begin
-            Parse_Edge (Line, N1, N2);
-            Add_Edge (Nodes, N1, N2);
-         end;
+      if Is_End (Node) then
+         Paths := Paths + 1;
+         return;
+      end if;
+      
+      if Node.Small then
+         if Node.Visited = 1 then
+            return;
+         else
+            Node.Visited := 1;
+         end if;
+      end if;
+      
+      for N of Node.Neighbors loop
+         Search_Paths (Nodes, N, Paths);
       end loop;
-      Close (File);
-   end Read_Data;
+      
+      Node.Visited := 0;
+   end Search_Paths;
 
    procedure Part_1 (File_Name : String) is
-      
-      function Start_Node (Nodes : Node_Vector.Vector)
-                          return Node_Index is
-      begin
-         for I in Nodes.First_Index .. Nodes.Last_Index loop
-            if Nodes.Element(I).Name = "start" then
-               return I;
-            end if;
-         end loop;
-         raise Value_Error with "Missing 'start' node";
-      end Start_Node;
-      
-      procedure Search_Paths (Nodes : in out Node_Vector.Vector;
-                              Index : Node_Index;
-                              Num_Paths : in out Natural) is
-      begin
-         if Nodes.Element(Index).Name = "end" then
-            Num_Paths := Num_Paths + 1;
-            return;
-         end if;
-         
-         if Nodes.Element(Index).Visited then
-            return;
-         end if;
-         
-         if Is_Small (Nodes.Element(Index)) then
-            Nodes.Reference(Index).Visited := True;
-         end if;
-         
-         for I of Nodes.Element(Index).Neighbors loop
-            if Nodes.Element(I).Name /= "start" then
-               Search_Paths (Nodes, I, Num_Paths);
-            end if;
-         end loop;
-         Nodes.Reference(Index).Visited := False;
-      end Search_Paths;
-      
-      -- Local variables
-      
       Nodes : Node_Vector.Vector;
-      Num_Paths : Natural := 0;
-      
-   -- Start of processing for Part_1
-      
+      Paths : Natural := 0;
    begin
       Read_Data (File_Name, Nodes);
-      -- Print_Nodes (Nodes);
-      Search_Paths (Nodes, Start_Node (Nodes), Num_Paths);
+      declare
+         Start_Node : Node_Access :=
+           Find_Node (Nodes, To_Unbounded_String ("start"));
+      begin
+         Search_Paths (Nodes, Start_Node, Paths);
+      end;
       
       Put_Line ("Part 1:");
-      Put_Line ("  Number of paths:" & Num_Paths'Img);
+      Put_Line ("  Number of paths:" & Paths'Img);
    end Part_1;
 
 begin
